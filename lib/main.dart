ddc290f5e2625d86867cad17309875252b7b3c84
Marco <ma.monzon@soprasteria.com>
2019-08-16 09:51:44 +0200

    feat: basic scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber[100],
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile-pic.jpeg'),
+              ),
+              Text(
+                'Marco Monzón',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.black54,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
