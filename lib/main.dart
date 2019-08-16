b3fa82fd636ac259e6e3a88ba2989b7661548460
Marco <ma.monzon@soprasteria.com>
2019-08-16 12:29:41 +0200

    feat: styles done

--- lib/main.dart
+++ lib/main.dart
@@ -11,32 +11,104 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 60.0,
-                backgroundImage: AssetImage('images/profile-pic.jpeg'),
-              ),
-              Text(
-                'Marco Monzón',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontFamily: 'Pacifico',
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blueGrey, Colors.blueGrey[100]],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.white,
+                          width: 3.0,
+                          style: BorderStyle.solid),
+                      borderRadius: BorderRadius.circular(100.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset(2, 2),
+                          blurRadius: 0.5,
+                        ),
+                      ]),
+                  child: CircleAvatar(
+                    radius: 60.0,
+                    backgroundImage: AssetImage('images/profile-pic.jpeg'),
+                  ),
+                ),
+                Text(
+                  'Marco Monzón',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'SOFTWARE DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                    color: Colors.blueGrey[100],
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                  width: 100.0,
+                  child: Divider(
+                    color: Colors.blueGrey[100],
+                  ),
+                ),
+                Card(
                   color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 30.0,
+                      color: Colors.blueGrey,
+                    ),
+                    title: Text(
+                      '+34 692 94 97 58',
+                      style: TextStyle(
+                        color: Colors.blueGrey[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              Text(
-                'SOFTWARE DEVELOPER',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'SourceSansPro',
-                  letterSpacing: 2.5,
-                  color: Colors.blueGrey[100],
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      size: 30.0,
+                      color: Colors.blueGrey,
+                    ),
+                    title: Text(
+                      'marcomonzip@gmail.com',
+                      style: TextStyle(
+                        color: Colors.blueGrey[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
